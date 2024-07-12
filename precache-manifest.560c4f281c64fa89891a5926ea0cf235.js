524613deed80b7ebc16183915de87b8f3c5be4eb
TMChi3n <tranminhchien15052003@gmail.com>
2024-07-12 10:24:54 +0700

    Updates

---
+++ precache-manifest.560c4f281c64fa89891a5926ea0cf235.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf421fbf6685c9994dc17e240ccd4e7",
+    "url": "/github.io/clothes-management/index.html"
+  },
+  {
+    "revision": "ce7962e55d6855253fa8",
+    "url": "/github.io/clothes-management/static/js/2.ca3240d3.chunk.js"
+  },
+  {
+    "revision": "bc97ee16332bb33bbf2173a5ca1eb54e",
+    "url": "/github.io/clothes-management/static/js/2.ca3240d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd4dffca7c3e9fc1e57",
+    "url": "/github.io/clothes-management/static/js/main.de01ad3a.chunk.js"
+  },
+  {
+    "revision": "ffe2486b7d24c3a4acc1",
+    "url": "/github.io/clothes-management/static/js/runtime-main.a8a935b2.js"
+  }
+]);
